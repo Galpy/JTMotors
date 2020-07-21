f1b87882db1ddc1debffba2299c6fb5e3ae12c37
Galpy <63217820+Galpy@users.noreply.github.com>
2020-07-21 13:54:00 -0500

    Add files via upload

---
+++ JTMotors-master/car5script.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides5(slideIndex);
+
+// Next/previous controls
+function plusSlides5(n) {
+  showSlides5(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide5(n) {
+  showSlides5(slideIndex = n);
+}
+
+function showSlides5(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides5");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
