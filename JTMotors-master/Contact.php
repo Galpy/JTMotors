9901d752c439e34887ddd52e707bbd0dfd96afe4
Galpy <63217820+Galpy@users.noreply.github.com>
2020-08-10 23:15:52 -0500

    Add files via upload

---
+++ JTMotors-master/Contact.php
@@ -0,0 +1,18 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "Gregglee2@yahoo.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['FirstName'];
+    $last_name = $_POST['LastName'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
---
+++ JTMotors-master/car3script.js
@@ -0,0 +1,26 @@
+
+var slideIndex = 1;
+showSlides3(slideIndex);
+
+// Next/previous controls
+function plusSlides3(n) {
+  showSlides3(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide3(n) {
+  showSlides3(slideIndex = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+}
